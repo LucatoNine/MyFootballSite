004b2924a93dc7374fd082e1e41a4b83a8ecc4c5
Ynask <104215253+LucatoNine@users.noreply.github.com>
2024-01-16 15:45:50 +0100

    Add main pages

--- FootballSite/FootballSite/listings/views.py
+++ FootballSite/FootballSite/listings/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+# Page d'accueil
+def home(request):
+    return (request, 'listings/home.html')
+
+# Page de présentation
+
+def about(request):
+    return (request, 'listings/about.html')
+
+# Page projet
+
+def project(request):
+    return (request, 'listings/project.html')
+
+# Page contact
+
+def contact(request):
+    return (request, 'listings/contact.html')
